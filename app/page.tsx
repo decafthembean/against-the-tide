e47543a1396f9fe169d5eb53e0c2e8801a8cd92b
kha <kha.tran.swe@gmail.com>
2026-02-08 23:19:05 +0700

    add article design choice

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Home() {
   return (
-      <div className="max-w-3xl m-auto font-mono">
-          <div>
+          <div className="max-w-3xl m-auto font-mono">
+              <div className="flex flex-col items-center gap-20 px-2 md:px-0">
+                  <Image src="/line.png" height={100} width={100} alt={"a pirate ship"} className={"dark:invert"} />
+{/*                  <article>
+                      <h3 className="font-sans text-2xl tracking-tighter text-center">What makes Kafka extremely fast?</h3>
+                      <div className="max-w-sm">
+                          <p className="font-mono text-sm mt-1 -indent-0 text-center">Kafka’s extreme throughput comes from simple design choices: an append-only log and efficient use of hardware.</p>
+                      </div>
+                      <h4 className="italic text-center text-xs font-geist-sans tracking-tight mt-4">Jan 8 2026 — 8 minutes read</h4>
+                  </article>*/}
+              </div>
           </div>
-      </div>
   );
 }
